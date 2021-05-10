ad7afd9ce93ef0a93954e7c69bb19a2e58b37f76
BrunoRodriguesN <bruno_adeira@hotmail.com>
2021-05-10 18:08:23 -0300

    ajustes finalizados no backend

--- src/main/java/com/bruno/bookstore/domain/Livro.java
+++ src/main/java/com/bruno/bookstore/domain/Livro.java
@@ -8,6 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -20,8 +23,18 @@ public class Livro implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotEmpty(message = "Campo TITULO é requerido")
+	@Length(min = 3 , max = 100, message = " O campo TITULO deve ter entre 3 e 100 caracteres")
 	private String titulo;
+	
+	@NotEmpty(message = "Campo NOME DO AUTOR é requerido")
+	@Length(min = 3 , max = 100, message = " O campo NOME DO AUTOR deve ter entre 3 e 100 caracteres")
 	private String nome_autor;
+	
+
+	@NotEmpty(message = "Campo NOME TEXTO é requerido")
+	@Length(min = 3 , max = 2000000, message = " O campo TEXTO deve ter entre 3 e 2000000 caracteres")	
 	private String texto;
 
 	// Iniciando relacionamento entre categoria e livros
